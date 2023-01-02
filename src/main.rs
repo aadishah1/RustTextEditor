b4bf41712afb11d915f38c70e527b5c1397ef656
aadishah1 <0blvn@protonmail.com>
2023-01-02 20:24:33 +0530

    attempt: adding Row struct

--- src/main.rs
+++ src/main.rs
@@ -169,7 +169,7 @@ impl Output {
                     self.editor_contents.push('~');
                 }
             } else {
-                let row = self.editor_rows.get_row(file_row);
+                let row = self.editor_rows.get_render(file_row);
                 let column_offset = self.cursor_controller.column_offset;
 
                 let len = if row.len() < column_offset {
@@ -246,9 +246,25 @@ impl Reader {
     }
 }
 
+// Used to store row contents and methods to
+// render them
+struct Row {
+    row_content: Box<str>,
+    render: String,
+}
+
+impl Row {
+    fn new(row_content: Box<str>, render: String) -> Self {
+        Self {
+            row_content,
+            render,
+        }
+    }
+}
+
 // Used to store contents of rows in the
 struct EditorRows {
-    row_contents: Vec<Box<str>>,
+    row_contents: Vec<Row>,
 }
 
 impl EditorRows {
@@ -263,21 +279,59 @@ impl EditorRows {
         }
     }
 
+    fn render_row(row: &mut Row) {
+        let mut index = 0;
+        let capacity = row
+            .row_content
+            .chars()
+            .fold(0, |acc, next| acc + if next == '\t' { 8 } else { 1 });
+
+        row.render = String::with_capacity(capacity);
+        row.row_content.chars().for_each(|c| {
+            index += 1;
+
+            if c == '\t' {
+                row.render.push(' ');
+                while index % 8 != 0 {
+                    row.render.push(' ');
+                    index += 1;
+                }
+            } else {
+                row.render.push(c);
+            }
+        });
+    }
+
     fn from_file(file: &Path) -> Self {
         let file_contents = fs::read_to_string(file).expect("Unable to read file");
 
         Self {
-            row_contents: file_contents.lines().map(|it| it.into()).collect(),
+            row_contents: file_contents
+                .lines()
+                .map(|it| {
+                    let mut row = Row::new(it.into(), String::new());
+                    Self::render_row(&mut row);
+                    row
+                })
+                .collect(),
         }
+    }
+
+    fn get_render(&self, at: usize) -> &String {
+        &self.row_contents[at].render
+    }
+
+    fn get_editor_row(&self, at: usize) -> &Row {
+        &self.row_contents[at]
     }
 
     fn number_of_rows(&self) -> usize {
         self.row_contents.len()
     }
 
-    fn get_row(&self, at: usize) -> &str {
-        &self.row_contents[at]
-    }
+    // fn get_row(&self, at: usize) -> &str {
+    //     &self.row_contents[at]
+    // }
 }
 
 // The actual text editor struct, includes the key
