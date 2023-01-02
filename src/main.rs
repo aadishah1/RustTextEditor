d9ac282dcee6e56fdb957ee2b212bc17f69cbd54
aadishah1 <0blvn@protonmail.com>
2023-01-02 22:23:33 +0530

    improvement: tab handling

--- src/main.rs
+++ src/main.rs
@@ -29,6 +29,7 @@ struct CursorController {
     screen_rows: usize,
     row_offset: usize,
     column_offset: usize,
+    render_x: usize,
 }
 
 impl CursorController {
@@ -40,7 +41,20 @@ impl CursorController {
             screen_rows: win_size.1,
             row_offset: 0,
             column_offset: 0,
+            render_x: 0,
         }
+    }
+
+    fn get_render_x(&self, row: &Row) -> usize {
+        row.row_content[..self.cursor_x]
+            .chars()
+            .fold(0, |render_x, c| {
+                if c == '\t' {
+                    render_x + (TAB_STOP - 1) - (render_x % TAB_STOP) + 1
+                } else {
+                    render_x + 1
+                }
+            })
     }
 
     fn move_cursor(&mut self, direction: KeyCode, editor_rows: &EditorRows) {
@@ -87,7 +101,12 @@ impl CursorController {
         self.cursor_x = cmp::min(self.cursor_x, row_len);
     }
 
-    fn scroll(&mut self) {
+    fn scroll(&mut self, editor_rows: &EditorRows) {
+        self.render_x = 0;
+        if self.cursor_y < editor_rows.number_of_rows() {
+            self.render_x = self.get_render_x(editor_rows.get_editor_row(self.cursor_y))
+        }
+
         // vertical scroll
         self.row_offset = cmp::min(self.row_offset, self.cursor_y);
         if self.cursor_y >= self.row_offset + self.screen_rows {
@@ -95,9 +114,9 @@ impl CursorController {
         }
 
         // horizontal scroll
-        self.column_offset = cmp::min(self.column_offset, self.cursor_x);
-        if self.cursor_x >= self.column_offset + self.screen_columns {
-            self.column_offset = self.cursor_x - self.screen_columns + 1;
+        self.column_offset = cmp::min(self.column_offset, self.render_x);
+        if self.render_x >= self.column_offset + self.screen_columns {
+            self.column_offset = self.render_x - self.screen_columns + 1;
         }
     }
 }
@@ -205,14 +224,14 @@ impl Output {
         // Hide the cursor before updates and relocate it to the top left
         // Show it back when update finishes
         // Also calls scroll
-        self.cursor_controller.scroll();
+        self.cursor_controller.scroll(&self.editor_rows);
         queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
 
         self.draw_rows();
 
         // Move the cursor to particular location based on
         // the cursor controller class
-        let cursor_x = self.cursor_controller.cursor_x - self.cursor_controller.column_offset;
+        let cursor_x = self.cursor_controller.render_x - self.cursor_controller.column_offset;
         let cursor_y = self.cursor_controller.cursor_y - self.cursor_controller.row_offset;
 
         queue!(
