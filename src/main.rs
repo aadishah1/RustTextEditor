a73021487f79c4c4bd967378ab91fc1187d3e813
aadishah1 <0blvn@protonmail.com>
2023-01-04 20:17:06 +0530

    feature: forward and backward searching

--- src/main.rs
+++ src/main.rs
@@ -218,6 +218,36 @@ impl StatusMessage {
     }
 }
 
+enum SearchDirection {
+    Forward,
+    Backward,
+}
+
+struct SearchIndex {
+    x_index: usize,
+    y_index: usize,
+    x_direction: Option<SearchDirection>,
+    y_direction: Option<SearchDirection>,
+}
+
+impl SearchIndex {
+    fn new() -> Self {
+        Self {
+            x_index: 0,
+            y_index: 0,
+            x_direction: None,
+            y_direction: None,
+        }
+    }
+
+    fn reset(&mut self) {
+        self.x_index = 0;
+        self.y_index = 0;
+        self.x_direction = None;
+        self.y_direction = None;
+    }
+}
+
 // Output struct is used to handle the output to the
 // terminal screen. This includes the ~ at the start of
 // each line like Vim and also used to ensure that
@@ -230,6 +260,7 @@ struct Output {
     editor_rows: EditorRows,
     status_message: StatusMessage,
     dirty: u64,
+    search_index: SearchIndex,
 }
 
 impl Output {
@@ -247,6 +278,7 @@ impl Output {
             editor_rows: EditorRows::new(),
             status_message: StatusMessage::new("Help: CTRL + S to Save | CTRL + F to Find | CTRL + Q to Quit.".into()),
             dirty: 0,
+            search_index: SearchIndex::new(),
         }
     }
 
@@ -333,12 +365,83 @@ impl Output {
 
     fn find_callback(output: &mut Output, keyword: &str, key_code: KeyCode) {
         match key_code {
-            KeyCode::Esc | KeyCode::Enter => {}
+            KeyCode::Esc | KeyCode::Enter => {
+                output.search_index.reset();
+            }
             _ => {
+                output.search_index.y_direction = None;
+                output.search_index.x_direction = None;
+
+                match key_code {
+                    KeyCode::Down => {
+                        output.search_index.y_direction = SearchDirection::Forward.into()
+                    }
+                    KeyCode::Up => {
+                        output.search_index.y_direction = SearchDirection::Backward.into()
+                    }
+                    KeyCode::Left => {
+                        output.search_index.x_direction = SearchDirection::Backward.into()
+                    }
+                    KeyCode::Right => {
+                        output.search_index.x_direction = SearchDirection::Forward.into()
+                    }
+                    _ => {}
+                }
+
                 for i in 0..output.editor_rows.number_of_rows() {
-                    let row = output.editor_rows.get_editor_row(i);
-                    if let Some(index) = row.render.find(&keyword) {
-                        output.cursor_controller.cursor_y = i;
+                    let row_index = match output.search_index.y_direction.as_ref() {
+                        None => {
+                            if output.search_index.x_direction.is_none() {
+                                output.search_index.y_index = i;
+                            }
+
+                            output.search_index.y_index
+                        }
+                        Some(dir) => {
+                            if matches!(dir, SearchDirection::Forward) {
+                                output.search_index.y_index + i + 1
+                            } else {
+                                let res = output.search_index.y_index.saturating_sub(i);
+                                if res == 0 {
+                                    break;
+                                }
+                                res - 1
+                            }
+                        }
+                    };
+
+                    if row_index > output.editor_rows.number_of_rows() - 1 {
+                        break;
+                    }
+
+                    let row = output.editor_rows.get_editor_row(row_index);
+                    let index = match output.search_index.x_direction.as_ref() {
+                        None => row.render.find(&keyword),
+                        Some(dir) => {
+                            let index = if matches!(dir, SearchDirection::Forward) {
+                                let start = cmp::min(row.render.len(),
+                                    output.search_index.x_index + 1);
+
+                                row.render[start..]
+                                    .find(&keyword)
+                                    .map(|index| index + start)
+                            } else{
+                                row.render[..output.search_index.x_index].rfind(&keyword)
+                            };
+
+                            if index.is_none() {
+                                break;
+                            }
+
+                            index
+                        }
+                    };
+
+                    if let Some(index) = index {
+                        output.cursor_controller.cursor_y = row_index;
+                        output.search_index.y_index = row_index;
+                        output.search_index.x_index = index;
+
                         output.cursor_controller.cursor_x = row.get_row_content_x(index);
                         output.cursor_controller.row_offset = output.editor_rows.number_of_rows();
                         break;
@@ -353,7 +456,7 @@ impl Output {
         
         if prompt!(
             self,
-            "Search: {} (ESC to cancel)",
+            "Search: {} (ESC to cancel, Arrows to find next matches, Enter to find)",
             callback = Output::find_callback
         ).is_none() {
             self.cursor_controller = cursor_controller;
